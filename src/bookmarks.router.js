e581105d64fea12ed4d0fd0d1e44d5905580c894
Daniel Sottile <danielsottile@gmail.com>
2020-05-01 14:45:31 -0400

    uuid is broken lul

--- src/bookmarks.router.js
+++ src/bookmarks.router.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { v4 : uuid} = require('uuid');
+const { v3 : uuid} = require('uuid');
 const logger = require('./logger');
 const { bookmarks }= require('./store');
 
